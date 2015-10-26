d4315a55cce0effaceff9523c40657e28c9180b9
kelvinmbwilo <kelvinmbwilo@gmial.com>
2015-10-26 17:38:53 +0300

    server updates

--- config/database.php
+++ config/database.php
@@ -55,9 +55,9 @@ return [
         'mysql' => [
             'driver'    => 'mysql',
             'host'      => env('DB_HOST', 'localhost'),
-            'database'  => env('DB_DATABASE', 'vvs'),
+            'database'  => env('DB_DATABASE', 'vssm'),
             'username'  => env('DB_USERNAME', 'root'),
-            'password'  => env('DB_PASSWORD', ''),
+            'password'  => env('DB_PASSWORD', 'kevdom'),
             'charset'   => 'utf8',
             'collation' => 'utf8_unicode_ci',
             'prefix'    => '',
